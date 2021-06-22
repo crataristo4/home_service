80ca30e5270d76afb891d830daed3aa004ef403e
Aristocrat <crataristo4@gmail.com>
2021-06-22 18:55:07 +0000

    added fetch data for gridView

--- lib/ui/views/artisan/view_artisan_by_category.dart
+++ lib/ui/views/artisan/view_artisan_by_category.dart
@@ -243,10 +243,10 @@ class _ViewArtisanByCategoryPageState extends State<ViewArtisanByCategoryPage>
     return StreamBuilder<List<DocumentSnapshot>>(
         stream: _artisanCategoryListBloc!.itemListStream,
         builder: (context, snapshot) {
-          debugPrint("Data : ${snapshot.data}");
           if (!snapshot.hasData) {
             return LoadingShimmer();
           } else {
+            debugPrint("Data : ${snapshot.data!.length}");
             return ListView.builder(
               shrinkWrap: true,
               itemCount: snapshot.data!.length,
@@ -292,17 +292,17 @@ class _ViewArtisanByCategoryPageState extends State<ViewArtisanByCategoryPage>
                                 children: [
                                   Column(
                                     crossAxisAlignment:
-                                        CrossAxisAlignment.start,
+                                    CrossAxisAlignment.start,
                                     children: [
                                       //artisan name
                                       Row(
                                         mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
+                                        MainAxisAlignment.spaceBetween,
                                         mainAxisSize: MainAxisSize.max,
                                         children: [
                                           Text(
                                             snapshot.data![index]
-                                                ['artisanName'],
+                                            ['artisanName'],
                                             style: TextStyle(
                                                 fontWeight: FontWeight.bold,
                                                 fontSize: sixteenDp),
@@ -364,11 +364,11 @@ class _ViewArtisanByCategoryPageState extends State<ViewArtisanByCategoryPage>
                             child: TextButton(
                                 style: TextButton.styleFrom(
                                     backgroundColor:
-                                        Theme.of(context).primaryColor,
+                                    Theme.of(context).primaryColor,
                                     primary: Colors.white,
                                     shape: RoundedRectangleBorder(
                                         borderRadius:
-                                            BorderRadius.circular(eightDp))),
+                                        BorderRadius.circular(eightDp))),
                                 onPressed: () {},
                                 child: Text(
                                   book,
@@ -387,156 +387,175 @@ class _ViewArtisanByCategoryPageState extends State<ViewArtisanByCategoryPage>
   }
 
   Widget buildGridCategory() {
-    return Container(
-        width: 300,
-        margin: EdgeInsets.only(right: fourDp, bottom: twelveDp),
-        // height: 500,
-        child: GridView.builder(
-            shrinkWrap: true,
-            itemCount: 50,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: twelveDp,
-              mainAxisSpacing: twelveDp,
-            ),
-            itemBuilder: (BuildContext context, int index) {
-              return GestureDetector(
-                child: Stack(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(left: eightDp),
-                      width: twoHundredDp,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(sixteenDp),
-                          border: Border.all(
-                              color: Colors.grey,
-                              width: 0.5,
-                              style: BorderStyle.solid)),
+    return StreamBuilder<List<DocumentSnapshot>>(
+        stream: _artisanCategoryListBloc!.itemListStream,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return LoadingShimmer();
+          } else {
+            return Container(
+                width: 300,
+                margin: EdgeInsets.only(right: fourDp, bottom: twelveDp),
+                // height: 500,
+                child: GridView.builder(
+                    shrinkWrap: true,
+                    itemCount: snapshot.data!.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: twelveDp,
+                      mainAxisSpacing: twelveDp,
                     ),
-                    //artisan details
-                    Positioned(
-                      top: 67,
-                      child: Container(
-                        margin: EdgeInsets.only(left: eightDp),
-                        width: 180,
-                        height: 120,
-                        decoration: BoxDecoration(
-                          color: Colors.indigo,
-                          borderRadius: BorderRadius.circular(sixteenDp),
-                        ),
-                        child: Padding(
-                          padding: EdgeInsets.only(
-                              top: sixteenDp, left: eightDp, right: eightDp),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              //artisan name
-                              Text(
-                                "Chantel Abrum",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
-                                    fontSize: sixteenDp),
-                              ),
-
-                              Row(
-                                mainAxisSize: MainAxisSize.min,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Flexible(
-                                    flex: 1,
-                                    fit: FlexFit.loose,
-                                    child: Padding(
-                                      padding: EdgeInsets.only(right: 20),
-                                      child: Text(
-                                        "30000 km",
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: sixteenDp,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                  RatingBar.builder(
-                                    itemPadding: EdgeInsets.only(top: 2),
-                                    itemSize: 14,
-                                    initialRating: 3,
-                                    minRating: 1,
-                                    direction: Axis.horizontal,
-                                    allowHalfRating: true,
-                                    itemCount: 5,
-                                    itemBuilder: (context, _) => Icon(
-                                      Icons.star,
-                                      color: Colors.amber,
-                                    ),
-                                    onRatingUpdate: (rating) {
-                                      print(rating);
-                                    },
-                                  ),
-                                ],
-                              ),
-                              Text(
-                                "5 years Experience",
-                                style: TextStyle(
-                                    fontSize: sixteenDp, color: Colors.grey),
-                              ),
-
-                              SizedBox(
-                                height: 38,
-                                width: MediaQuery.of(context).size.width,
-                                child: Container(
-                                  margin: EdgeInsets.only(
-                                      left: eightDp,
-                                      right: eightDp,
-                                      bottom: fourDp,
-                                      top: fourDp),
-                                  child: TextButton(
-                                      style: TextButton.styleFrom(
-                                          backgroundColor: Colors.white,
-                                          primary: Colors.indigo,
-                                          shape: RoundedRectangleBorder(
-                                              borderRadius:
-                                              BorderRadius.circular(
-                                                  eightDp))),
-                                      onPressed: () {},
-                                      child: Text(
-                                        book,
-                                        style: TextStyle(fontSize: fourteenDp),
-                                      )),
+                    itemBuilder: (BuildContext context, int index) {
+                      return GestureDetector(
+                        child: Stack(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(left: eightDp),
+                              width: twoHundredDp,
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius:
+                                      BorderRadius.circular(sixteenDp),
+                                  border: Border.all(
+                                      color: Colors.grey,
+                                      width: 0.5,
+                                      style: BorderStyle.solid)),
+                            ),
+                            //artisan details
+                            Positioned(
+                              top: 67,
+                              child: Container(
+                                margin: EdgeInsets.only(left: eightDp),
+                                width: 180,
+                                height: 120,
+                                decoration: BoxDecoration(
+                                  color: Colors.indigo,
+                                  borderRadius:
+                                      BorderRadius.circular(sixteenDp),
                                 ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
+                                child: Padding(
+                                  padding: EdgeInsets.only(
+                                      top: sixteenDp,
+                                      left: eightDp,
+                                      right: eightDp),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      //artisan name
+                                      Text(
+                                        snapshot.data![index]['artisanName'],
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white,
+                                            fontSize: sixteenDp),
+                                      ),
 
-                    //artisan profile image
-                    Positioned(
-                      top: 10,
-                      left: 50,
-                      right: 50,
-                      child: ClipOval(
-                        clipBehavior: Clip.antiAlias,
-                        child: Container(
-                          // margin: EdgeInsets.only(right: tenDp, top: tenDp),
-                          width: sixtyDp,
-                          height: seventyDp,
-                          decoration: BoxDecoration(
-                            // border: Border.all(width: 2,color: Colors.white54),
-                              image: DecorationImage(
-                                fit: BoxFit.cover,
-                                image: AssetImage(
-                                    "assets/images/a.png"), //todo -load image from network
-                              )),
+                                      Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Flexible(
+                                            flex: 1,
+                                            fit: FlexFit.loose,
+                                            child: Padding(
+                                              padding:
+                                                  EdgeInsets.only(right: 20),
+                                              child: Text(
+                                                "30000 km",
+                                                style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: sixteenDp,
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                          RatingBar.builder(
+                                            itemPadding:
+                                                EdgeInsets.only(top: 2),
+                                            itemSize: 14,
+                                            initialRating: 3,
+                                            minRating: 1,
+                                            direction: Axis.horizontal,
+                                            allowHalfRating: true,
+                                            itemCount: 5,
+                                            itemBuilder: (context, _) => Icon(
+                                              Icons.star,
+                                              color: Colors.amber,
+                                            ),
+                                            onRatingUpdate: (rating) {
+                                              print(rating);
+                                            },
+                                          ),
+                                        ],
+                                      ),
+                                      Text(
+                                        snapshot.data![index]['expLevel'],
+                                        style: TextStyle(
+                                            fontSize: sixteenDp,
+                                            color: Colors.grey),
+                                      ),
+
+                                      SizedBox(
+                                        height: 38,
+                                        width:
+                                            MediaQuery.of(context).size.width,
+                                        child: Container(
+                                          margin: EdgeInsets.only(
+                                              left: eightDp,
+                                              right: eightDp,
+                                              bottom: fourDp,
+                                              top: fourDp),
+                                          child: TextButton(
+                                              style: TextButton.styleFrom(
+                                                  backgroundColor: Colors.white,
+                                                  primary: Colors.indigo,
+                                                  shape: RoundedRectangleBorder(
+                                                      borderRadius:
+                                                          BorderRadius.circular(
+                                                              eightDp))),
+                                              onPressed: () {},
+                                              child: Text(
+                                                book,
+                                                style: TextStyle(
+                                                    fontSize: fourteenDp),
+                                              )),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+
+                            //artisan profile image
+                            Positioned(
+                              top: 10,
+                              left: 50,
+                              right: 50,
+                              child: ClipOval(
+                                clipBehavior: Clip.antiAlias,
+                                child: Container(
+                                  // margin: EdgeInsets.only(right: tenDp, top: tenDp),
+                                  width: sixtyDp,
+                                  height: seventyDp,
+                                  decoration: BoxDecoration(
+                                      // border: Border.all(width: 2,color: Colors.white54),
+                                      image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: CachedNetworkImageProvider(
+                                        snapshot.data![index]['photoUrl']),
+                                  )),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            }));
+                      );
+                    }));
+          }
+        });
   }
 
   Widget buildMapItem() {
