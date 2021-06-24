605ef952d387040caa8ba9f9f6dbfa2ba9cc2582
Aristocrat <crataristo4@gmail.com>
2021-06-24 17:08:50 +0000

    added view_all_artisans.dart

--- lib/ui/views/artisan/view_all_artisans.dart
+++ lib/ui/views/artisan/view_all_artisans.dart
@@ -1,4 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:home_service/ui/models/users.dart';
 import 'package:provider/provider.dart';
 
@@ -18,46 +21,137 @@ class _ViewAllArtisansState extends State<ViewAllArtisans> {
     final artisanListProvider = Provider.of<List<Artisans>>(context);
 
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
         backgroundColor: Colors.white,
-        leading: InkWell(
-          onTap: () => Navigator.pop(context),
-          child: Container(
-            margin: EdgeInsets.all(tenDp),
-            decoration: BoxDecoration(
-                border: Border.all(width: 0.3, color: Colors.grey),
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(thirtyDp)),
-            child: Padding(
-              padding: EdgeInsets.all(eightDp),
-              child: Icon(
-                Icons.arrow_back_ios,
-                color: Colors.grey,
-                size: sixteenDp,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          leading: InkWell(
+            onTap: () => Navigator.pop(context),
+            child: Container(
+              margin: EdgeInsets.all(tenDp),
+              decoration: BoxDecoration(
+                  border: Border.all(width: 0.3, color: Colors.grey),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(thirtyDp)),
+              child: Padding(
+                padding: EdgeInsets.all(eightDp),
+                child: Icon(
+                  Icons.arrow_back_ios,
+                  color: Colors.grey,
+                  size: sixteenDp,
+                ),
               ),
             ),
           ),
+          title: Text(
+            allAvailableArtisans,
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: sixteenDp,
+                fontWeight: FontWeight.bold),
+          ),
         ),
-        title: Text(
-          allAvailableArtisans,
-          style: TextStyle(
-              color: Colors.black,
-              fontSize: sixteenDp,
-              fontWeight: FontWeight.bold),
-        ),
-      ),
-      body: Builder(builder: (BuildContext context) {
-        return ListView.builder(
-          itemBuilder: (context, index) {
-            return Text(artisanListProvider[index].artisanName);
-          },
-          itemCount: artisanListProvider.length,
-          shrinkWrap: true,
-          primary: true,
-        );
-      }),
-    );
+        body: Builder(builder: (BuildContext context) {
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              return Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: eightDp),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        border:
+                            Border.all(width: 20, color: Colors.indigoAccent)),
+                    child: ListTile(
+                      onTap: () {
+                        //1.pass artisans details
+                        //todo
+                        //2.navigate to artisan's profile
+                      },
+                      minVerticalPadding: 25,
+                      horizontalTitleGap: 4,
+                      tileColor: Colors.indigoAccent,
+                      title: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          //artisans name
+                          Text(
+                            artisanListProvider[index].artisanName,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          //todo implement location
+                          Text(
+                            "3 km",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(top: sixDp, bottom: sixDp),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Flexible(
+                                  //artisan's category
+                                  flex: 1,
+                                  child: Text(
+                                      artisanListProvider[index].category,
+                                      style: TextStyle(color: Colors.white)),
+                                ),
+                                Flexible(
+                                  //todo implement rating logic
+                                  child: RatingBar.builder(
+                                    itemPadding: EdgeInsets.only(top: 2),
+                                    itemSize: 14,
+                                    initialRating: 3,
+                                    minRating: 1,
+                                    direction: Axis.horizontal,
+                                    allowHalfRating: true,
+                                    itemCount: 5,
+                                    itemBuilder: (context, _) => Icon(
+                                      Icons.star,
+                                      color: Colors.amber,
+                                    ),
+                                    onRatingUpdate: (rating) {
+                                      print(rating);
+                                    },
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Text(
+                            // artisan's experience
+                            artisanListProvider[index].expLevel,
+                            style: TextStyle(color: Colors.white),
+                          )
+                        ],
+                      ),
+                      leading: Padding(
+                        padding: EdgeInsets.only(top: eightDp),
+                        child: CircleAvatar(
+                          radius: 40,
+                          backgroundImage: CachedNetworkImageProvider(
+                              artisanListProvider[index].photoUrl),
+                          backgroundColor: Colors.indigo,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  )
+                ],
+              );
+            },
+            itemCount: artisanListProvider.length,
+            shrinkWrap: true,
+          );
+        }));
   }
 }
