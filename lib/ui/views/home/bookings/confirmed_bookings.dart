a8a47eede6332303cdcce9fbc632b5c1f9c9f905
Aristocrat <crataristo4@gmail.com>
2021-06-25 20:07:55 +0000

    added confirmed_bookings.dart and pending_booking.dart

--- lib/ui/views/home/bookings/confirmed_bookings.dart
+++ lib/ui/views/home/bookings/confirmed_bookings.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:home_service/models/booking.dart';
+import 'package:provider/provider.dart';
+
+import '../../../../constants.dart';
 
 class ConfirmedBookings extends StatefulWidget {
+  static const routeName = '/confirmedBookings';
+
   const ConfirmedBookings({Key? key}) : super(key: key);
 
   @override
@@ -10,6 +16,85 @@ class ConfirmedBookings extends StatefulWidget {
 class _ConfirmedBookingsState extends State<ConfirmedBookings> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final confirmedBookingList = Provider.of<List<Bookings>>(context);
+    return Builder(
+      builder: (BuildContext context) {
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            return Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: eightDp),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      border:
+                          Border.all(width: 20, color: Colors.indigoAccent)),
+                  child: ListTile(
+                    onTap: () {
+                      //1.pass artisans details
+                      //todo
+                      //2.navigate to artisan's profile
+                    },
+                    minVerticalPadding: 25,
+                    horizontalTitleGap: 4,
+                    tileColor: Colors.indigoAccent,
+                    title: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        //artisans name
+                        Text(
+                          confirmedBookingList[index].artisanName,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(top: sixDp, bottom: sixDp),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Flexible(
+                                //
+                                flex: 1,
+                                child: Text(confirmedBookingList[index].message,
+                                    style: TextStyle(color: Colors.white)),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Text(
+                          //  experience
+                          confirmedBookingList[index].status,
+                          style: TextStyle(color: Colors.white),
+                        )
+                      ],
+                    ),
+                    /* leading: Padding(
+                      padding: EdgeInsets.only(top: eightDp),
+                      child: CircleAvatar(
+                        radius: 40,
+                        backgroundImage: CachedNetworkImageProvider(
+                            artisanListProvider[index].photoUrl),
+                        backgroundColor: Colors.indigo,
+                      ),
+                    ),*/
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                )
+              ],
+            );
+          },
+          itemCount: confirmedBookingList.length,
+          shrinkWrap: true,
+        );
+      },
+    );
   }
 }
--- lib/ui/views/home/bookings/pending_bookings.dart
+++ lib/ui/views/home/bookings/pending_bookings.dart
@@ -1,15 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:home_service/models/booking.dart';
+import 'package:provider/provider.dart';
 
-class PendingBookings extends StatefulWidget {
+import '../../../../constants.dart';
+
+class PendingBookings extends StatelessWidget {
+  static const routeName = '/pendingBookings';
+
   const PendingBookings({Key? key}) : super(key: key);
 
   @override
-  _PendingBookingsState createState() => _PendingBookingsState();
-}
-
-class _PendingBookingsState extends State<PendingBookings> {
-  @override
   Widget build(BuildContext context) {
-    return Container();
+    final pendingBookingList = Provider.of<List<Bookings>>(context);
+    print("pending list ${pendingBookingList.length}");
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Builder(
+        builder: (BuildContext context) {
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              return Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: eightDp),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        border:
+                            Border.all(width: 20, color: Colors.indigoAccent)),
+                    child: ListTile(
+                      onTap: () {
+                        //1.pass artisans details
+                        //todo
+                        //2.navigate to artisan's profile
+                      },
+                      minVerticalPadding: 25,
+                      horizontalTitleGap: 4,
+                      tileColor: Colors.indigoAccent,
+                      title: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          //artisans name
+                          Text(
+                            pendingBookingList[index].artisanName,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(top: sixDp, bottom: sixDp),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Flexible(
+                                  //
+                                  flex: 1,
+                                  child: Text(pendingBookingList[index].message,
+                                      style: TextStyle(color: Colors.white)),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Text(
+                            //  experience
+                            pendingBookingList[index].status,
+                            style: TextStyle(color: Colors.white),
+                          )
+                        ],
+                      ),
+                      /* leading: Padding(
+                        padding: EdgeInsets.only(top: eightDp),
+                        child: CircleAvatar(
+                          radius: 40,
+                          backgroundImage: CachedNetworkImageProvider(
+                              artisanListProvider[index].photoUrl),
+                          backgroundColor: Colors.indigo,
+                        ),
+                      ),*/
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  )
+                ],
+              );
+            },
+            itemCount: pendingBookingList.length,
+            shrinkWrap: true,
+          );
+        },
+      ),
+    );
   }
 }
